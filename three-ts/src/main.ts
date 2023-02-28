13bb9bc04c44c3765706aba51819fde9623d6c04
Thomas Hodges <thodges@javascriptonline.org>
2023-02-28 11:24:03 -0800

    first ts version

---
+++ three-ts/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css';
+
+import { initThreeJsScene } from './threeCanvas';
+
+const mainElement = document.querySelector<HTMLDivElement>("#app");
+if (mainElement) {
+  initThreeJsScene(mainElement);
+}
