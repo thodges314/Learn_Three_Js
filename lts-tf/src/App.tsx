4f08cb6eedf2f7f0e8f8c7eff65d3de06caf1705
Thomas Hodges <thodges@javascriptonline.org>
2023-02-28 11:56:40 -0800

    typescript + react

---
+++ lts-tf/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { ThreeCanvas } from './ThreeCanvas';
+
+function App() {
+  return (
+    <div className="App">
+      <ThreeCanvas />
+    </div>
+  );
+}
+
+export default App;
